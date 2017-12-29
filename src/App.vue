a5559e92f51c148ec0d3f1ca4fd77a48355ef155
Saul Florez <saulflorez.backend@gmail.com>
2017-12-28 20:54:36 -0400

    Manipulación del DOM | Integración a Platzi Music

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,57 @@
 <template lang="pug">
   #app
-    input(v-model="name")     
-    button(v-on:click="format") Format
-
-    p {{ formattedName }}
+    section.section
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(
+            type="text",
+            placeholder="Buscar canciones", 
+            v-model="searchQuery"
+          )
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-danger.is-large &times;
+          p
+            small {{ searchMessage }}
+    
+      .container.results
+        .columns
+          .column(v-for="t in tracks") {{ t.name }} - {{ t.artist }}
 </template>
 
 <script>
+const tracks = [
+  { name: 'Muchacha', artist: 'Luis Alberto Sinetta' },
+  { name: 'Hoy aca en el baile', artist: 'El pepo' },
+  { name: 'I was made for loving you', artist: 'Kiss' }
+]
+
 export default {
   name: 'app',
   data () {
     return {
-      name: '',
-      formattedName: ''
+      searchQuery: '',
+      tracks: []
+    }
+  },
+
+  computed: {
+    searchMessage () {
+      return `Encontrados: ${this.tracks.length}`
     }
   },
 
   methods: {
-    format () {
-      this.formattedName = this.name.split(' ').join('-').toUpperCase()
+    search () {
+      this.tracks = tracks
     }
   }
 }
 </script>
 
 <style lang="scss">
-@import './scss/main.scss'
+  @import './scss/main.scss';
+  
+  .results {
+    margin-top: 50px;
+  }
 </style>
