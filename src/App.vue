f8ca30cc11073d3a2a16e22a1b6252d080c2a01c
sherzo <saulflorez.backend@gmail.com>
2018-02-07 20:14:08 -0430

    Sistema de componentes | Utilización de slots

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
   #app
     pm-header
+  
+    pm-notification(v-show="showNotification")
+      p(slot="body") No se encontraron resultados
 
     pm-loader(v-show="isLoading")
     section.section(v-show="!isLoading")
@@ -32,26 +35,38 @@
 
 <script>
 import trackService from '@/services/track'
+
 import PmFooter from '@/components/layout/Footer.vue'
 import PmHeader from '@/components/layout/Header.vue'
 
 import PmTrack from '@/components/Track.vue'
+
+import PmNotification from '@/components/shared/Notification.vue'
 import PmLoader from '@/components/shared/Loader.vue'
 
 export default {
   name: 'app',
-  components: { PmFooter, PmHeader, PmTrack, PmLoader },
+  components: { PmFooter, PmHeader, PmTrack, PmLoader, PmNotification },
   data () {
     return {
       searchQuery: '',
       tracks: [],
 
       isLoading: false,
+      showNotification: false,
 
       selectedTrack: ''
     }
   },
-
+  watch: {
+    showNotification () {
+      if (this.showNotification) {
+        setTimeout(() => {
+          this.showNotification = false
+        }, 3000)
+      }
+    }
+  },
   computed: {
     searchMessage () {
       return `Encontrados: ${this.tracks.length}`
@@ -65,6 +80,7 @@ export default {
       this.isLoading = true
       trackService.search(this.searchQuery)
         .then(res => {
+          this.showNotification = res.tracks.total === 0
           this.tracks = res.tracks.items
           this.isLoading = false
         })
